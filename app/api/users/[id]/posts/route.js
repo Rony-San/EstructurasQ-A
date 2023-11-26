c8ebbbd003f2aad215b8651ad8ffc41251d17d1e
Rony-san <rony.banol@udea.edu.co>
2023-11-26 18:17:59 -0500

    the before commit dont work v1

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,15 +1,18 @@
 import Question from "@models/question";
 import { connectToDB } from "@utils/database";
 
-export const dynamic = "force-dynamic";
-export const GET = async (request) => {
+export const GET = async (request, { params }) => {
   try {
     await connectToDB();
 
-    const questions = await Question.find({}).populate("creator");
+    const questions = await Question.find({ creator: params.id }).populate(
+      "creator"
+    );
 
     return new Response(JSON.stringify(questions), { status: 200 });
   } catch (error) {
-    return new Response("Failed to fetch all questions", { status: 500 });
+    return new Response("Failed to fetch prompts created by user", {
+      status: 500,
+    });
   }
 };
