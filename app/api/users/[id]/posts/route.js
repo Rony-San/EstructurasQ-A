213433d13fc0eef512b708e4fac71c9b3a01b942
Rony-san <rony.banol@udea.edu.co>
2023-11-26 18:10:46 -0500

    trying forum solution

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,18 +1,15 @@
 import Question from "@models/question";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request, { params }) => {
+export const dynamic = "force-dynamic";
+export const GET = async (request) => {
   try {
     await connectToDB();
 
-    const questions = await Question.find({ creator: params.id }).populate(
-      "creator"
-    );
+    const questions = await Question.find({}).populate("creator");
 
     return new Response(JSON.stringify(questions), { status: 200 });
   } catch (error) {
-    return new Response("Failed to fetch prompts created by user", {
-      status: 500,
-    });
+    return new Response("Failed to fetch all questions", { status: 500 });
   }
 };
