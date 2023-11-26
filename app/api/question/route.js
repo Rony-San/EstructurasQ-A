ab1155e48268ec685992b90ada14be66e4ee408c
Rony-san <rony.banol@udea.edu.co>
2023-11-26 17:14:29 -0500

    revalidating the cache

--- app/api/question/route.js
+++ app/api/question/route.js
@@ -7,7 +7,12 @@ export const GET = async (request) => {
 
     const questions = await Question.find({}).populate("creator");
     console.log("esta es la pregunta" + questions);
-    return new Response(JSON.stringify(questions), { status: 200 });
+    return new Response(JSON.stringify(questions), {
+      status: 200,
+      headers: {
+        "Cache-Control": "no-store, must-revalidate",
+      },
+    });
   } catch (error) {
     return new Response("fallo al traer todas las questions", { status: 500 });
   }
