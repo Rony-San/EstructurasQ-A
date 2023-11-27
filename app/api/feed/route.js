90a10f700febf70cd72deb98c73995374b7c471e
Rony-san <rony.banol@udea.edu.co>
2023-11-26 20:46:32 -0500

    let's see changing the ip feed

--- app/api/feed/route.js
+++ app/api/feed/route.js
@@ -1,16 +1,34 @@
 import Question from "@models/question";
-import { connectToDB } from "@utils/database";
+import { connectToDB, disconnectFromDB } from "@utils/database";
 
 export const GET = async (request) => {
   try {
+    // Conéctate a la base de datos
     await connectToDB();
 
+    // Realiza la consulta de preguntas y popula el campo "creator"
     const questions = await Question.find().populate("creator");
-    console.log("esta es la pregunta" + questions);
+
+    // Desconéctate de la base de datos después de realizar la consulta
+    // await disconnectFromDB();
+
+    // Log para verificar las preguntas obtenidas
+    console.log("Estas son las preguntas:", questions);
+
+    // Devuelve la respuesta al cliente
     return new Response(JSON.stringify(questions), {
       status: 200,
+      headers: {
+        "Cache-Control": "no-store, no-cache, must-revalidate, max-age=0",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
     });
   } catch (error) {
-    return new Response("fallo al traer todas las questions", { status: 500 });
+    // Manejo de errores en caso de fallo
+    console.error("Error al traer todas las preguntas:", error);
+
+    // Devuelve una respuesta de error al cliente
+    return new Response("Fallo al traer todas las preguntas", { status: 500 });
   }
 };
