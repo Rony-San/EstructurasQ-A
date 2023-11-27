c1a3b0e8304f29b192f76d2aefe07bbf31efaa50
Rony-san <rony.banol@udea.edu.co>
2023-11-26 19:34:52 -0500

    organizando la api

--- app/api/question/route.js
+++ app/api/question/route.js
@@ -6,13 +6,10 @@ export const GET = async (request) => {
   try {
     await connectToDB();
 
-    const questions = await Question.find({}).populate("creator");
+    const questions = await Question.find(any).populate("creator");
     console.log("esta es la pregunta" + questions);
     return new Response(JSON.stringify(questions), {
       status: 200,
-      headers: {
-        "Cache-Control": "no-store, must-revalidate",
-      },
     });
   } catch (error) {
     return new Response("fallo al traer todas las questions", { status: 500 });
