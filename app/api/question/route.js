e3dc4d515d887bda404037cab7acfcbb60b3f4e5
Rony-san <rony.banol@udea.edu.co>
2023-11-26 21:16:49 -0500

    forum changing

--- app/api/question/route.js
+++ app/api/question/route.js
@@ -1,11 +1,14 @@
 import Question from "@models/question";
 import { connectToDB } from "@utils/database";
 
+export const dynamic = "force-dynamic";
+
+export const revalidate = 1; //revalidate api every 1 second
 export const GET = async (request) => {
   try {
     await connectToDB();
 
-    const questions = await Question.find().populate("creator");
+    const questions = await Question.find({}).populate("creator");
     console.log("esta es la pregunta" + questions);
     return new Response(JSON.stringify(questions), {
       status: 200,
